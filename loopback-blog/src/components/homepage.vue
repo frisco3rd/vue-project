14685c23959571f0f4ba5ce948e8308d2cba4353
Dwight <carter91jr@gmail.com>
2018-10-19 16:14:54 -0400

    Working on edit method

--- loopback-blog/src/components/homepage.vue
+++ loopback-blog/src/components/homepage.vue
@@ -6,10 +6,13 @@
             <div class="card mb-5">
                 <div class="card-body">
                     <div class="form-group mb-3">
-                        <input type="text" name="" id="" class="form-control" placeholder="Enter Title" v-model="name">
+                        <input type="text" name="" id="" class="form-control" placeholder="Enter your Name" v-model="name">
                         </div>
                         <div class="form-group mb-3">
-                            <textarea name="" id="" cols="30" rows="10" class="form-control" placeholder="Enter your content" v-model="city"></textarea>
+                            <textarea name="" id="" cols="30" rows="10" class="form-control" placeholder="Enter your City" v-model="city"></textarea>
+                        </div>
+                                                <div class="form-group mb-3">
+                            <textarea name="" id="" cols="30" rows="10" class="form-control" placeholder="Enter your Address" v-model="address"></textarea>
                         </div>
                         <button class="btn btn-primary" v-on:click="addPost">ADD</button>
                     </div>
@@ -17,16 +20,19 @@
 
             </div>
         </div>
-                    <h2> Post List </h2>
+                    <h2>Client List</h2>
                 <div class="card mb-3" v-for="meets in meetups">
                     <div class="card-body">
                          <h3>{{meets.name}}</h3>
                         <p v-text="meets.city"></p>
                         <p v-text="meets.address"></p>
-                         <p v-text="meets.id"></p>
+                        <!-- Use for testing -->
+                         <!-- <p v-text="meets.id"></p> -->
 
                         </div>
                         <button class="btn btn-danger" v-on:click="deletePost(meets.id)">Delete</button>
+                        <button class="btn btn-safe" v-on:click="updatePost(meets.id)">Edit</button>
+
 
                 </div>
 </div>
@@ -45,12 +51,13 @@ export default {
       meetups: [],
       name: "",
       city: "",
+      address: "",
       id:""
     };
   },
   mounted() {
       this.getMeetups()
-      console.log(this.meetups)
+      console.log(this.meet)
 
   },
   methods: {
@@ -58,7 +65,7 @@ export default {
           const data = {
               name: this.name,
               city: this.city,
-              
+              address: this.address
               }
           axios.post('http://localhost:3000/api/meetups', data).then(response =>{
               this.getMeetups()
@@ -76,6 +83,16 @@ export default {
           this.getMeetups()
       })
     },
+    // Working on update method
+    //     updatePost(id){
+    //   axios.put("http://localhost:3000/api/meetups/" + id ).then(response => {
+    //       this.name = 
+    //       this.city = 
+    //       this.address =
+    //       this.id = 
+    //       this.getMeetups()
+    //   })
+    // },
     clearInput(){
         this.name = ''
         this.city = ''
