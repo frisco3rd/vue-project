807cf0e587733d4b3e4c55c66c93c02588847aa0
Dwight <carter91jr@gmail.com>
2018-10-19 12:26:18 -0400

    working add and remove buttons but elemet adds back on refresh

--- loopback-blog/src/components/homepage.vue
+++ loopback-blog/src/components/homepage.vue
@@ -1,16 +1,82 @@
 <template>
-<h2>{{message}}</h2>
+
+<div class="container">
+    <div class="row justify-content-center my-5">
+        <div class="col-6">
+            <div class="card mb-5">
+                <div class="card-body">
+                    <div class="form-group mb-3">
+                        <input type="text" name="" id="" class="form-control" placeholder="Enter Title" v-model="name">
+                        </div>
+                        <div class="form-group mb-3">
+                            <textarea name="" id="" cols="30" rows="10" class="form-control" placeholder="Enter your content" v-model="city"></textarea>
+                        </div>
+                        <button class="btn btn-primary" v-on:click="addPost">ADD</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+                    <h2> Post List </h2>
+                <div class="card mb-3" v-for="meets in meetups">
+                    <div class="card-body">
+                         <h3>{{meets.name}}</h3>
+                        <p v-text="meets.city"></p>
+                        <p v-text="meets.address"></p>
+                        </div>
+                <button class="btn btn-danger" v-on:click="deletePost">Delete</button>
+
+
+                </div>
+</div>
+
 </template>
 
 
+
 <script>
+import axios from "axios";
 
 export default {
-    data(){
-        return {
-            message:"Hello world"
-        }
+  data() {
+    return {
+      message: "Hello world",
+      meetups: [],
+      name: "",
+      city: "",
+      id:""
+    };
+  },
+  mounted() {
+      this.getMeetups()
+  },
+  methods: {
+      addPost(){
+          const data = {
+              name: this.name,
+              city: this.city,
+              }
+          axios.post('http://localhost:3000/api/meetups', data).then(response =>{
+              this.getMeetups()
+              this.clearInput()
+          })
+      },
+    getMeetups() {
+      axios.get("http://localhost:3000/api/meetups/").then(response => {
+        this.meetups = response.data;
+        this.meetups.reverse()
+      })
+    },
+    deletePost(id){
+      axios.delete("http://localhost:3000/api/meetups/" + id).then(response => {
+          this.meetups.splice(id,1)
+        console.log(response)
+      })
+    },
+    clearInput(){
+        this.name = ''
+        this.city = ''
     }
-}
-
+  }
+};
 </script>
